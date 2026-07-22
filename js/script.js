98b80c4a69471abd408e96184d802c6475b12119
jade roland eduard <jaderolandeduard.pinon@lorma.edu>
2026-07-22 20:02:03 +0800

    made varying colors for the difficulty pills

--- js/script.js
+++ js/script.js
@@ -17,9 +17,10 @@ parseJson().then(renderQuizCards);
 function renderQuizCards() {
   const quizList = document.querySelector(".quiz-list");
   const cards = quizzes.map((quiz) => {
+    const difficulty = quiz.category.toLowerCase();
     return `
       <div class="quiz-card">
-        <span class="pill">${quiz.category}</span>
+        <span class="pill ${difficulty}">${quiz.category}</span>
         <h3 class="quiz-title">${quiz.title}</h3>
         <p class="quiz-meta">${quiz.amount} questions</p>
       </div>
